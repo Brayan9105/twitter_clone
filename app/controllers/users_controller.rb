b5a2f0bbcaca65522de699c544768ef3a3a1363e
Brayan Lopez <brayan.lopez@koombea.com>
2020-06-27 13:18:29 -0500

    Added search user method to user controller and model + routes adn view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,30 @@ class UsersController < ApplicationController
     @user = User.find(params[:user_id])
     @tweets = @user.tweets
   end
+
+  def search_user
+  end
+
+  def search
+    if params[:username].present?
+      @users = User.search(params[:username])
+      @users = current_user.except_current_user(@users)
+      if @users
+        respond_to do |format|
+          format.js { render partial: 'users/user_result' }
+        end
+      else
+        respond_to do |format|
+          flash.now[:alert] = 'Could not find a user'
+          format.js { render partial: 'users/user_result' }
+        end
+      end
+    else
+      respond_to do |format|
+        flash.now[:alert] = 'Please enter a username'
+        format.js { render partial: 'users/user_result' }
+      end
+    end
+    
+  end
 end
