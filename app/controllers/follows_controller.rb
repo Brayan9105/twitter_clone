3082e4d6aef236be7352c8b4ffc82b0caab0be7a
Brayan Lopez <brayan.lopez@koombea.com>
2020-06-29 13:25:13 -0500

    follow controller frindly id

--- app/controllers/follows_controller.rb
+++ app/controllers/follows_controller.rb
@@ -1,13 +1,13 @@
 class FollowsController < ApplicationController
   def create
-    user = User.find(params[:id])
+    user = User.friendly.find(params[:id])
     follow = Follow.new(user_id: current_user.id, following_id: user.id)
     follow.save
     redirect_to root_path
   end
 
   def destroy
-    user = User.find(params[:id])
+    user = User.friendly.find(params[:id])
     follow = Follow.where(following_id: user.id, user_id: current_user.id).first
     follow.destroy
     redirect_to root_path
